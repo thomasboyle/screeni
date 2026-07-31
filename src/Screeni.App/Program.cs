37fee6c2432e59938a2993e4e4ea092405fd61b3
Thomas Boyle <thomas.paul.boyle@gmail.com>
2026-07-31 15:48:56 +0100

    fix: polish icons and enforce single instance

--- src/Screeni.App/Program.cs
+++ src/Screeni.App/Program.cs
@@ -6,12 +6,23 @@ namespace Screeni;
 
 public static class Program
 {
+    private const string SingleInstanceMutexName = @"Local\Screeni.SingleInstance";
+
     [DllImport("Microsoft.ui.xaml.dll")]
     private static extern void XamlCheckProcessRequirements();
 
     [STAThread]
     public static void Main(string[] args)
     {
+        using var singleInstanceMutex = new Mutex(
+            initiallyOwned: true,
+            name: SingleInstanceMutexName,
+            createdNew: out var createdNew);
+        if (!createdNew)
+        {
+            return;
+        }
+
         XamlCheckProcessRequirements();
         ComWrappersSupport.InitializeComWrappers();
         Application.Start(_ => new App());
